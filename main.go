8b310b81ed821d87dd59bd019f3ac8fe518e2522
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-10-06 16:32:30 +0900

    support http3

--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
+
+	"github.com/lucas-clemente/quic-go/http3"
 )
 
-func main(){
+var (
+	crt = "tls/server.crt"
+	key = "tls/server.key"
+)
+
+func main() {
 	port := ":44300"
 
 	http.HandleFunc("/", handler)
-	log.Println("listening https://localhost" + port)
 
-	if err:= http.ListenAndServeTLS(port,"tls/server.crt", "tls/server.key",nil);err!=nil{
+	version := flag.Int("version", 3, "http version")
+	flag.Parse()
+
+	var err error
+	switch *version {
+	case 3:
+		log.Println("listening http3 on https://localhost" + port)
+		err = http3.ListenAndServeQUIC(port, crt, key, nil)
+	case 2:
+		log.Println("listening http2 on https://localhost" + port)
+		err = http.ListenAndServeTLS(port, crt, key, nil)
+	}
+
+	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	dump,err:=httputil.DumpRequest(r,true)
-	if err!=nil{
-		http.Error(w,fmt.Sprint(err),http.StatusInternalServerError)
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
 	}
 	fmt.Println(string(dump))
-	fmt.Fprintf(w,"ok")
+	fmt.Fprintf(w, "ok")
 }
