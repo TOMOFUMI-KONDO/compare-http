8b12092c37833697e98960829fe08408dc3fce73
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-10-09 16:52:51 +0900

    http/1.1 client

---
+++ client/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	// load certificate
+	cert, err := ioutil.ReadFile("tls/ca.crt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(cert)
+	//tlsConfig := &tls.Config{RootCAs: certPool}
+	tlsConfig := &tls.Config{InsecureSkipVerify: true}
+	tlsConfig.BuildNameToCertificate()
+
+	// create client with certificate
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	resp, err := client.Get("https://localhost:44300")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(dump))
+}
