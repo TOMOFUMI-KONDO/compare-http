7f076ab3325db90e70cf0d8275bd42d5220c3e6d
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-10-09 22:24:40 +0900

    output also private ip

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -24,12 +24,20 @@ resource "aws_instance" "client" {
   }
 }
 
-output "server_address" {
+output "server_public_ip" {
   value = aws_instance.server.public_ip
 }
 
-output "client_address" {
+output "server_private_ip" {
+  value = aws_instance.server.private_ip
+}
+
+output "client_public_ip" {
   value = aws_instance.client.public_ip
+}
+
+output "client_private_ip" {
+  value = aws_instance.client.private_ip
 }
 
 resource "aws_security_group" "ec2" {
