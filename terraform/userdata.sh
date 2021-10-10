ac0ebeda1733751257074efde0e471b58335c3c4
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-10-10 14:38:32 +0900

    update userdata

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -2,3 +2,8 @@
 
 yum install -y git
 git clone https://github.com/TOMOFUMI-KONDO/compare-http.git /home/ec2-user/compare-http
+chown -R ec2-user:ec2-user /home/ec2-user/compare-http
+
+# Raise UDP receive buffer size for QUIC
+# https://github.com/lucas-clemente/quic-go/wiki/UDP-Receive-Buffer-Size
+sysctl -w net.core.rmem_max=2500000
