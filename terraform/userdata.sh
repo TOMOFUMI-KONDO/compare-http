ff30c02b2f88da9bd3357ade6e2da48267313343
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-10-10 14:30:03 +0900

    fix git clone dst

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yum install -y git
-git clone https://github.com/TOMOFUMI-KONDO/compare-http.git /home/ec2-user
+git clone https://github.com/TOMOFUMI-KONDO/compare-http.git /home/ec2-user/compare-http
