3e103ddb23913fe161d2a570838bc7faefb27ca7
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-10-20 21:30:02 +0900

    replaced library that plot histogram

--- continuoussend/chart/chart.go
+++ continuoussend/chart/chart.go
@@ -1,95 +1,48 @@
 package chart
 
 import (
-	"flag"
 	"fmt"
-	"os"
 	"time"
 
-	"github.com/go-echarts/go-echarts/v2/charts"
-	"github.com/go-echarts/go-echarts/v2/opts"
+	"gonum.org/v1/plot/vg"
+
+	"gonum.org/v1/plot/plotter"
+
+	"gonum.org/v1/plot"
 )
 
 var (
-	outDir = "out"
-
+	outDir    = "out"
 	latencies = make([]int, 0)
-
-	classMax int
-	line     = charts.NewLine()
 )
-
-func init() {
-	flag.IntVar(&classMax, "max", 100, "max of latency class")
-	flag.Parse()
-}
 
 func Record(latency int) {
 	latencies = append(latencies, latency)
 }
 
 func Render() error {
-	filename := time.Now().Format("2006-01-02_15:04:05") + ".html"
-	f, err := makeFile(outDir, filename)
+	p := plot.New()
+	p.Title.Text = "Latency Histogram"
+
+	values := make(plotter.Values, len(latencies))
+	for i, v := range latencies {
+		values[i] = float64(v)
+	}
+
+	h, err := plotter.NewHist(values, 100)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
+	p.Add(h)
 
-	line.SetGlobalOptions(
-		charts.WithTitleOpts(opts.Title{Title: "Latency Histogram"}),
-		charts.WithXAxisOpts(opts.XAxis{Name: "latency [ms]"}),
-		charts.WithYAxisOpts(opts.YAxis{Name: "frequency"}),
-	)
+	filename := time.Now().Format("2006-01-02_15:04:05") + ".png"
+	file := outDir + "/" + filename
 
-	line.SetXAxis(makeRange(classMax)).
-		AddSeries("HTTP/1.1", makeLines(latencies, classMax)).
-		SetSeriesOptions(
-			charts.WithAreaStyleOpts(opts.AreaStyle{Opacity: 0.2}),
-		)
-
-	if err = line.Render(f); err != nil {
+	if err = p.Save(4*vg.Inch, 4*vg.Inch, file); err != nil {
 		return err
 	}
 
-	fmt.Printf("rendered to %s\n", f.Name())
+	fmt.Printf("rendered to %s\n", file)
 
 	return nil
-}
-
-func makeRange(max int) []int {
-	r := make([]int, max)
-	for i := 1; i < max+1; i++ {
-		r[i-1] = i
-	}
-	return r
-}
-
-func makeFile(dir, file string) (*os.File, error) {
-	if err := os.MkdirAll(dir, os.FileMode(0755)); err != nil {
-		return nil, err
-	}
-
-	f, err := os.Create(dir + "/" + file)
-	if err != nil {
-		return nil, err
-	}
-
-	return f, nil
-}
-
-func makeLines(data []int, max int) []opts.LineData {
-	items := make([]int, max)
-
-	for _, d := range data {
-		prev := items[d]
-		items[d] = prev + 1
-	}
-
-	bars := make([]opts.LineData, len(items))
-	for i := 0; i < len(items); i++ {
-		bars[i] = opts.LineData{Value: items[i]}
-	}
-
-	return bars
 }
--- continuoussend/go.mod
+++ continuoussend/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-echarts/go-echarts/v2 v2.2.4
 	github.com/lucas-clemente/quic-go v0.23.0
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f
+	gonum.org/v1/plot v0.10.0 // indirect
 )
